0e6bd66ffb0d7fe5fe191d3b53cec4b94815e60a
luis <luisfelorivas@gmai.com>
2022-04-27 15:48:09 -0500

    Replace getter setter and contractors for annotation @Data from lombok.

--- src/main/java/com/crud/democrud/models/UsuarioModel.java
+++ src/main/java/com/crud/democrud/models/UsuarioModel.java
@@ -2,12 +2,14 @@ package com.crud.democrud.models;
 
 
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Data
 @Entity
 @Table(name = "usuario")
 public class UsuarioModel implements Serializable {
@@ -26,46 +28,5 @@ public class UsuarioModel implements Serializable {
     @ManyToMany(mappedBy = "usuarios")
     private List<UsuarioRolModel> roles=new ArrayList<>();
 
-    public void setPrioridad(Integer prioridad) {
-        this.prioridad = prioridad;
-    }
-
-    public Integer getPrioridad() {
-        return prioridad;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public UsuarioModel(String nombre, String email, Integer prioridad) {
-        this.nombre = nombre;
-        this.email = email;
-        this.prioridad = prioridad;
-    }
-
-    public UsuarioModel() {
-
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
